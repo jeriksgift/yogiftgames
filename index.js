17c87d62ea14416d502394a493f7ea21146f75b0
jeriksgift <jeriksgift@gmail.com>
2024-10-01 21:59:55 +0530

    changed media query to js

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-// var photoelement = document.getElementById("my_photo")
+// var photoelement = document.getElementById("my_photo");
 // photoelement.style.backgroundColor = '#8a007e';
 
 // function old (){
 
 // }
+var aboutme = document.getElementById("about_me");
+var info = document.getElementById("info");
+var contact = document.getElementById("contact");
+
+window.addEventListener('resize',function(){
+    var w = window.innerWidth;
+    var h = window.innerHeight;    
+    
+    if (w<h){           //Potrait
+        aboutme.style.flexDirection = 'column';
+        info.style.fontSize = 'large';
+        contact.style.fontSize = 'large';
+    }
+    else{
+        aboutme.style.flexDirection = 'row';
+        info.style.fontSize = 'medium';
+        contact.style.fontSize = 'medium';
+    }
+});
