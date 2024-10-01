84e3983c04ff2bc460b8741850f156f8a5ac254d
jeriksgift <jeriksgift@gmail.com>
2024-10-01 22:12:13 +0530

    Moved the hover from css to js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var aboutme = document.getElementById("about_me");
 var info = document.getElementById("info");
 var contact = document.getElementById("contact");
+var photo = document.getElementById("my_photo");
+var sa = document.getElementById("stay_alive");
+var bf = document.getElementById("be_fast");
 
 window.addEventListener('resize',function(){
     var w = window.innerWidth;
@@ -16,4 +19,22 @@ window.addEventListener('resize',function(){
         info.style.fontSize = 'medium';
         contact.style.fontSize = 'medium';
     }
+});
+photo.addEventListener('mouseover',function(){
+    photo.style.backgroundColor = '#eb196a';
+});
+photo.addEventListener('mouseleave', function(){
+    photo.style.backgroundColor = '#8a007e';
+});
+sa.addEventListener('mouseover',function(){
+    sa.style.backgroundColor = '#eb196a';
+});
+sa.addEventListener('mouseleave', function(){
+    sa.style.backgroundColor = '#8a007e';
+});
+bf.addEventListener('mouseover',function(){
+    bf.style.backgroundColor = '#eb196a';
+});
+bf.addEventListener('mouseleave', function(){
+    bf.style.backgroundColor = '#8a007e';
 });
