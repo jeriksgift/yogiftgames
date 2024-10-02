5c291b2c78cb101c38544dbe2df4cb56ea6bb13b
jeriksgift <jeriksgift@gmail.com>
2024-10-02 14:47:14 +0530

    Changed the arrangement of games listed, added platform sections for games and gave them title

--- index.js
+++ index.js
@@ -4,37 +4,50 @@ var contact = document.getElementById("contact");
 var photo = document.getElementById("my_photo");
 var sa = document.getElementById("stay_alive");
 var bf = document.getElementById("be_fast");
+var w = window.innerWidth;
+var h = window.innerHeight;
 
-window.addEventListener('resize',function(){
-    var w = window.innerWidth;
-    var h = window.innerHeight;    
-    
-    if (w<h){           //Potrait
+if (w < h) {           //Potrait
+    aboutme.style.flexDirection = 'column';
+    info.style.fontSize = 'large';
+    contact.style.fontSize = 'large';
+}
+else {
+    aboutme.style.flexDirection = 'row';
+    info.style.fontSize = 'medium';
+    contact.style.fontSize = 'medium';
+}
+
+window.addEventListener('resize', function () {
+    w = window.innerWidth;
+    h = window.innerHeight;
+
+    if (w < h) {           //Potrait
         aboutme.style.flexDirection = 'column';
         info.style.fontSize = 'large';
         contact.style.fontSize = 'large';
     }
-    else{
+    else {
         aboutme.style.flexDirection = 'row';
         info.style.fontSize = 'medium';
         contact.style.fontSize = 'medium';
     }
 });
-photo.addEventListener('mouseover',function(){
+photo.addEventListener('mouseover', function () {
     photo.style.backgroundColor = '#eb196a';
 });
-photo.addEventListener('mouseleave', function(){
+photo.addEventListener('mouseleave', function () {
     photo.style.backgroundColor = '#8a007e';
 });
-sa.addEventListener('mouseover',function(){
+sa.addEventListener('mouseover', function () {
     sa.style.backgroundColor = '#eb196a';
 });
-sa.addEventListener('mouseleave', function(){
+sa.addEventListener('mouseleave', function () {
     sa.style.backgroundColor = '#8a007e';
 });
-bf.addEventListener('mouseover',function(){
+bf.addEventListener('mouseover', function () {
     bf.style.backgroundColor = '#eb196a';
 });
-bf.addEventListener('mouseleave', function(){
+bf.addEventListener('mouseleave', function () {
     bf.style.backgroundColor = '#8a007e';
 });
